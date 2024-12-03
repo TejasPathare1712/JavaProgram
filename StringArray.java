b5ad5e30f559e0502024c60e6c19992dd2d69f5b
TejasPathare1712 <96852800+TejasPathare1712@users.noreply.github.com>
2024-12-03 09:55:22 +0530

    Update StringArray.java

--- StringArray.java
+++ StringArray.java
@@ -11,3 +11,32 @@ class StringArray{
 		}
 	}
 }
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+import java.util.Scanner;
+class Main{
+    public static void Length(String arr[]){
+        for(int i=0;i<10;i++){
+            if(arr[i].length()==5){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+	public static void main(String args[]){
+		System.out.println("-----Friends List-------");
+		//String friends[] =  {"Atharv","Virendra","Nagesh","Vishnu","Harshad","Bhagwat"};
+		/*for(int i=0;i<=friends.length-1;i++){
+			System.out.println(friends[i]);
+		}*/
+		Scanner sc = new Scanner(System.in);
+		String arr[] = new String[10];
+		for(int i=0;i<10;i++){
+		    arr[i]=sc.nextLine();
+		}
+		System.out.println("No of friends whose length is 5");
+		Main.Length(arr);
+		//System.out.println("Lengths of Friends = "+friends.length);
+		//for(String j:friends){
+		//	System.out.println(j);
+		//}
+	}
+}
